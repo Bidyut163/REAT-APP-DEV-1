3f7aa551d7203383a92ad90d90612436e38b383a
Bidyut Deka <bidyutranjan464@gmail.com>
2022-05-09 11:10:02 +0530

    table created with associations

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const sequelize = require('./util/database');
 
 const app = express();
 
+// Models
+const Appellant = require('./models/Appellant');
+const Appeal = require('./models/Appeal');
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
@@ -13,14 +17,22 @@ app.use(express.json({ extended: false }));
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const appellantUserRoutes = require('./routes/appellants/userRoutes');
+const appealRoutes = require('./routes/appellants/appealRoutes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/appellants', appellantUserRoutes);
+app.use('/api/appellants/appeal', appealRoutes);
 
+// Define PORT
 const PORT = process.env.PORT || 5000;
 
+// Model relations
+Appeal.belongsTo(Appellant, { constraints: true, onDelete: 'CASCADE' });
+Appellant.hasMany(Appeal);
+
 sequelize
+    // .sync({ force: true })
     .sync()
     .then((result) => {
         app.listen(PORT, () => {
